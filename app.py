e0626e6ea448807d5b3e52c2aec1d99108175c10
Ganesh Tripathi <ganesha_trip@yahoo.co.in>
2022-07-24 22:43:06 +0530

    is even odd

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# Number identifier App
+
+This app identifies the entered number is odd or even
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    number = st.number_input("NUMBER",min_value=-99,max_value=9999999999999)
+    
+    data = {'NUMBER': number           
+            }
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df.to_dict())
+
+st.write(pd.DataFrame({'Result': ['Even','Odd']}))
+st.subheader('RESULT')
+num = df.iloc[0][0]
+if (num % 2) == 0:
+    st.write('The entered number is EVEN')
+else:
+    st.write('The entered number is ODD')
---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"22ds2000187@student.onlinedegree.iitm.ac.in\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
