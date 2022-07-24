90549402d9edc5bbcefb55c99d448f164275de3b
Ganesh Tripathi <ganesha_trip@yahoo.co.in>
2022-07-24 23:51:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ This app identifies the entered number is odd or even
 st.header('User Input Parameters')
 num = st.number_input('Enter the number')
 st.subheader('RESULT')
-if (num % 2) == 0:
-    st.write('The entered number is EVEN')
+if (num.is_integer()):
+    if (num % 2) == 0:
+        st.write('The entered number is EVEN')
+    else:
+        st.write('The entered number is ODD') 
 else:
-    st.write('The entered number is ODD')
+    st.write('Please Enter whole number')
