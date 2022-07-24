a3a7baa3388e88ea070e200fbf1d0686da6da577
Ganesh Tripathi <ganesha_trip@yahoo.co.in>
2022-07-24 22:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-import pickle
 
 st.write("""
 # Number identifier App
